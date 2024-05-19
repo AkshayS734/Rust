00713c09cb43423f256db4c7d95ce7e42077f66d
Akshay Shukla <manasshukla876@gmail.com>
2024-05-19 18:48:00 +0530

    Till loops added

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+
+    //Hello World
+    println!("Hello, world!");
+
+    //Initializing numbers
+    let x = -5;
+    let y: u32 = 1;
+    let z: f32 = 1000.32;
+    println!("x: {},y: {},z: {}",x,y,z);
+
+    //Initializing Strings
+    let str = String::from("Hello, Rust");
+    println!("{}",str);
+
+    //Conditional Statements
+    let is_even= true;
+    if is_even {
+        println!("Number is even");
+    }else if !is_even {
+        println!("Number is odd");
+    }
+
+    //Loops
+    for i in 0..11 {
+        print!("{} ",i);
+    }
+}
